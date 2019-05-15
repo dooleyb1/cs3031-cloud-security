f307e81b3fccf238a917554962ad7805e9e6a1e5
dooleyb1 <dooleyb1@tcd.ie>
2019-05-15 16:43:32 +0100

    pushing final changes

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyC83wP63a0uQmyMf_MxKBixNTuvP4H3mFc",
+    authDomain: "cs3031-securing-the-cloud.firebaseapp.com",
+    databaseURL: "https://cs3031-securing-the-cloud.firebaseio.com",
+    projectId: "cs3031-securing-the-cloud",
+    storageBucket: "cs3031-securing-the-cloud.appspot.com",
+    messagingSenderId: "223005514381"
+  };
+
+export default config;
